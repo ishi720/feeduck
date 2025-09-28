3459878c1db2978bfef0595638951673c8a1b4d4
ishi720 <ishizuka720@gmail.com>
2025-09-28 10:54:36 +0900

    ルーティングをクラスベースで書き換え

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,35 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\RssController;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\HomeController;
 
 
-Route::get('/', function () {
-  return redirect('/search');
-});
+Route::redirect('/', '/search');
 
-Route::get('/registration', 'RssController@registration')->name('registration');
-Route::post('/registration','RssController@registration')->name('registration');
+// 登録画面
+Route::get('/registration', [RssController::class, 'registration'])->name('registration');
+Route::post('/registration', [RssController::class, 'registration'])->name('registration');
 
-Route::get('/search', 'RssController@search')->name('search');
-Route::post('/search', 'RssController@search')->name('search');
+// 検索画面
+Route::get('/search', [RssController::class, 'search'])->name('search');
+Route::post('/search', [RssController::class, 'search'])->name('search');
 
-Route::get('/detail', 'RssController@detail')->name('detail');
+// RSS詳細画面
+Route::get('/detail', [RssController::class, 'detail'])->name('detail');
 
-Route::get('/feedlist', 'RssController@feedlist')->name('feedlist');
-Route::post('/feedlist', 'RssController@feedlist')->name('feedlist');
+// Feed一覧画面
+Route::get('/feedlist', [RssController::class, 'feedlist'])->name('feedlist');
+Route::post('/feedlist', [RssController::class, 'feedlist'])->name('feedlist');
 
+// RSS情報編集画面
+Route::get('/edit', [RssController::class, 'edit'])->name('edit');
+Route::post('/edit', [RssController::class, 'edit'])->name('edit');
+
+// 認証
+Auth::routes();
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// API
 Route::get('/api/get', 'ApiController@RssGet');
 Route::get('/api/set', 'ApiController@RssSet');
-
-Route::get('/edit', 'RssController@edit')->name('edit');
-Route::post('/edit', 'RssController@edit')->name('edit');
-
-
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
